0c2b03de2e76d7ea76adcc9ce3ebddbcecdc4afd
Nikhil KVK <nikhil.kvk.24@gmail.com>
2024-11-02 00:31:20 +0530

    Implemented Orders API

--- firstapi.py
+++ firstapi.py
@@ -6,10 +6,12 @@ import pymongo
 import logging
 from bson import json_util
 import json
+import pymongo.collation
+import pymongo.collection
 import uvicorn
 from pymongo import MongoClient
-from datetime import datetime
-
+from datetime import datetime, timedelta
+from bson import ObjectId
 
 app = FastAPI()
 logger = logging.getLogger('uvicorn.error')
@@ -32,11 +34,48 @@ class MenuCard(BaseModel):
 	vegItems:str
 	
 class Order(BaseModel):
-	menuItems:str	
+	menuItems:str
+	userId:str	
+
+
 
 tasks = []
 dbclient = pymongo.MongoClient("mongodb://localhost:27017/")
 dataBase = dbclient["BookUrMeal"]
+
+
+@app.get("/getLast7DaysOrders/{emailId}")
+def getTodaysMenu(emailId:str): 
+	mdMenus = dataBase["Orders"]
+	todayDt = datetime.today()
+	query = {"$and": [{"createdOn": {"$gte": (todayDt - timedelta(days=7)).strftime('%Y-%m-%d'), "$lte": todayDt.strftime('%Y-%m-%d')}}, {"userId": emailId}]}
+	print(query)
+	resp = "{\"orders\":["
+	resultSet = mdMenus.find(query)
+	
+	firstDoc : bool = True
+	for document in resultSet:
+		print(document)
+		del document["_id"]
+		if firstDoc:
+			resp = resp+json.dumps(document)
+			firstDoc = False
+		else:
+			resp = resp+","+json.dumps(document)
+		
+	resp = resp+"]}"
+	res = json.loads(resp)
+	print(str(res))
+	return res
+	# print(json.dumps(finaldict))
+
+	# return CusJSONEncoder().encode(finaldict)
+
+class CusJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 
 @app.post("/createOrder/")
@@ -58,6 +97,7 @@ def getTodaysMenu():
 	print(query)
 	resultSet = mdMenus.find_one(query)
 	del resultSet["_id"]
+	print(type(resultSet))
 	return resultSet
 
 @app.post("/addItemsOnTodaysMenu/")
@@ -125,6 +165,7 @@ def update_task(task_id: int, updated_task: Task):
 		return updated_task
 	else:
 		raise HTTPException(status_code=404, detail="Task not found")
+	
 @app.delete("/tasks/{task_id}", response_model=Task)
 def delete_task(task_id: int):
 	if 0 <= task_id < len(tasks):
